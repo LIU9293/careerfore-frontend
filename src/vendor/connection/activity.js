7d26df19ad7503f895990d38a85aa8caf6a2629d
LIU9293 <mylock.kai@gmail.com>
2016-08-22 13:11:11 +0800

    clear connection api

---
+++ src/vendor/connection/activity.js
@@ -0,0 +1,40 @@
+import { getData, getDataBase64 } from './basis.js';
+
+//获取活动首页列表
+export const getPlaygroundList = (AreaID, pageCurrent, pageSize, callback) => {
+  let queryObj = {
+    areaID: AreaID,
+    PageCurrent: pageCurrent,
+    PageSize: pageSize
+  }
+  getDataBase64('ZQ.APP.ActivityManage.GetActivityList', queryObj, (err, data)=>{
+    if(err){
+      callback(err)
+    } else {
+      if (data.ResultCode === 1){
+        callback(null, data.ActivityList);
+      } else {
+        callback(data.ResultMessage);
+      }
+    }
+  })
+}
+
+//获取活动详情
+export const getPlaygroundPost = (id, userid, callback) => {
+  let queryObj = {
+    ActivityID: id,
+    UserID: userid
+  };
+  getDataBase64('ZQ.APP.ActivityManage.GetActivityByID', queryObj, (err, data)=>{
+    if(err){
+      callback(err)
+    } else {
+      if (data.ResultCode === 1){
+        callback(null, data);
+      } else {
+        callback(data.ResultMessage);
+      }
+    }
+  })
+}
