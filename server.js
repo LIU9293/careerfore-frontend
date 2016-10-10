6743284624dadb236253a7eba59899353d365ae0
liu9293 <mylock.kai@gmail.com>
2016-10-10 11:48:48 +0800

    a

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const path = require('path')
-const port = process.env.PORT || 8989
+const port = process.env.PORT || 80
 const app = express()
 
 // 通常用于加载静态资源
 app.use(express.static(__dirname + '/dist'))
+
+app.get('/about', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'dist', 'About.html'))
+})
+
+app.get('/recruitment', function (request, response){
+  response.sendFile(path.resolve(__dirname, 'dist', 'Recruitment.html'))
+})
 
 // 在你应用 JavaScript 文件中包含了一个 script 标签
 // 的 index.html 中处理任何一个 route
