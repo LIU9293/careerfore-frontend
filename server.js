4dd7cf6078300281decfc2f06fd64b9a81593732
careerfore <careerfore@careerforedeMac-mini.local>
2016-10-10 11:34:10 +0800

    add

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const port = process.env.PORT || 8989
-const app = express
+const app = express()
 
 var bodyParser = require('body-parser');
 var crypto = require('crypto')
